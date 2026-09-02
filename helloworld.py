f181b82baa2049e0c9814db8b77516a176b6c3e5
Shafiq <shafiq.jalawan@gmail.com>
2026-09-02 00:12:31 -0500

    Update Hello World program

--- helloworld.py
+++ helloworld.py
@@ -1,8 +1,23 @@
 name = "Alice"
 age = 32
-a = 12
-b = 16
 
-print(f"The sum of {a} and {b} is {a + b}.")
-print(f"Hello, World! My name is {name}, and I am {age} years old.")
 print("Welcome to Python programming!")
+
+print(f"Hello, World! My name is {name}, and I am {age} years old.")
+
+operations = "Operations"
+
+print( operations + " :")
+
+
+a = 10
+b = 5.10
+
+sum = a + b
+print(f"The sum of {a} and {b} is: {sum}")
+difference = a - b
+print(f"The difference between {a} and {b} is: {difference}")   
+multiplication = a * b
+print(f"The multiplication of {a} and {b} is: {multiplication}")
+division = a / b
+print(f"The division of {a} by {b} is: {division}")
