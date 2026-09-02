d66787bb45081146f8a4db5a14dc1b275ea49fd2
Shafiq <shafiq.jalawan@gmail.com>
2026-09-02 00:11:42 -0500

    Update variable values and add sum calculation

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,8 @@
 name = "Alice"
 age = 32
+a = 12
+b = 16
 
+print(f"The sum of {a} and {b} is {a + b}.")
 print(f"Hello, World! My name is {name}, and I am {age} years old.")
 print("Welcome to Python programming!")
